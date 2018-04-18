9511525f62dadc8393c73c9d481d2a573a3bcc06
Unknown <recuero.david@gmail.com>
2018-04-18 09:58:17 +0200

    Punt 1 fet

--- code/src/physics.cpp
+++ code/src/physics.cpp
@@ -1,57 +1,115 @@
 #include <imgui\imgui.h>
 #include <imgui\imgui_impl_sdl_gl3.h>
+#include <GL\glew.h>
+#include <glm\gtc\type_ptr.hpp>
+#include <glm\gtc\matrix_transform.hpp>
+#include <cstdio>
+#include <cassert>
+#include <stdlib.h>     /* srand, rand */
+#include <time.h>       /* time */
+
+#include "GL_framework.h"
+
+//http://www.opengl-tutorial.org/beginners-tutorials/tutorial-3-matrices/
+//https://stackoverflow.com/questions/8844585/glm-rotate-usage-in-opengl
+//Mirar de ferho amb quaternions
+
+//Fer Rand Decimals
+//Establir vertex
+//Detectar col.lisions dels vertexs amb parets
+//Establir nou estat després de col.lisió
 
 bool show_test_window = false;
 bool reset = false;
 
-namespace Box {
+glm::vec3 gravity{ 0.f, -9.81f, 0.f };
+glm::vec3 initPos{ 0.f, 10.f, 0.f };
+
+glm::vec3 force;
+glm::vec3 cubePos;
+
+glm::mat4 cubeMatPos = glm::translate(glm::mat4(1.f), cubePos);
+glm::mat4 cubeMatRotX = glm::mat4(1.f);
+glm::mat4 cubeMatRotY = glm::mat4(1.f);
+glm::mat4 cubeMatRotZ = glm::mat4(1.f);
+
+float rx, ry, rz; //random angles rotation
+
+namespace Cube {
 	void setupCube();
 	void cleanupCube();
+	void updateCube(const glm::mat4& transform);
 	void drawCube();
+
 }
 
 void GUI() {
 	bool show = true;
 	ImGui::Begin("Physics Parameters", &show, 0);
 
-	// Do your GUI code here....
-	{	
+	{
 		ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);//FrameRate
-		
+
 		if (ImGui::Button("Reset Simulation"))	reset = true;
 	}
-	// .........................
-	
+
 	ImGui::End();
 
-	// Example code -- ImGui test window. Most of the sample code is in ImGui::ShowTestWindow()
-	if(show_test_window) {
+	if (show_test_window) {
 		ImGui::SetNextWindowPos(ImVec2(650, 20), ImGuiSetCond_FirstUseEver);
 		ImGui::ShowTestWindow(&show_test_window);
 	}
 }
 
 void PhysicsInit() {
-	// Do your initialization code here...
-	// ...................................
 
-	Box::setupCube();
+	cubePos = initPos;
+	force = glm::vec3{ static_cast <float> (rand() % 10 - 5), 
+					   static_cast <float> (rand() % 10 - 5), 
+					   static_cast <float> (rand() % 10 - 5) };
+
+	rx = static_cast <float> (rand() % 1 - 0.5);
+	ry = static_cast <float> (rand() % 1 - 0.5);
+	rz = static_cast <float> (rand() % 1 - 0.5);
+
+	Cube::setupCube();
 }
 
 void PhysicsUpdate(float dt) {
-	// Do your update code here...
-	// ...........................
 
-	Box::drawCube();
+	force += gravity*dt;
+	cubePos += force*dt;
+
+	cubeMatPos = glm::translate(glm::mat4(1.f), cubePos);
+
+	cubeMatRotX = glm::rotate(cubeMatRotX, rx, glm::vec3{ 1, 0, 0 });
+	cubeMatRotY = glm::rotate(cubeMatRotY, ry, glm::vec3{ 0, 1, 0 });
+	cubeMatRotZ = glm::rotate(cubeMatRotZ, rz, glm::vec3{ 0, 0, 1 });
 
 	if (reset)
 	{
 		//reset things
 
+		cubePos = initPos;
+		
+		force = glm::vec3{ static_cast <float> (rand() % 10 - 5), static_cast <float> (rand() % 10 - 5), static_cast <float> (rand() % 10 - 5) };
+
+		rx = static_cast <float> (rand() % 2 - 1);
+		ry = static_cast <float> (rand() % 2 - 1);
+		rz = static_cast <float> (rand() % 2 - 1);
+
+		cubeMatPos = glm::translate(glm::mat4(1.f), cubePos);
+		cubeMatRotX = glm::rotate(cubeMatRotX, rx, glm::vec3{ 1, 0, 0 });
+		cubeMatRotY = glm::rotate(cubeMatRotY, ry, glm::vec3{ 0, 1, 0 });
+		cubeMatRotZ = glm::rotate(cubeMatRotZ, rz, glm::vec3{ 0, 0, 1 });
+
 		reset = false;
 	}
+
+	Cube::updateCube(cubeMatPos * (cubeMatRotX * cubeMatRotY * cubeMatRotZ));	//REVISAR ABANS D'ENTREGAR
+	Cube::drawCube();
 }
 
 void PhysicsCleanup() {
-	Box::cleanupCube();
+	Cube::cleanupCube();
 }
