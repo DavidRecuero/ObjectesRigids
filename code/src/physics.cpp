ccc3c4c57a35adad13d845205c4baa0402f62958
DavidRecuero <recuero.david@gmail.com>
2018-04-24 02:30:31 +0200

    Col·lisions i decimals aleatoris

--- code/src/physics.cpp
+++ code/src/physics.cpp
@@ -18,9 +18,9 @@
 
 //Mirar de ferho amb quaternions
 
-//Fer Rand Decimals
-//Establir vertex
-//Detectar col.lisions dels vertexs amb parets
+//Fer Rand Decimals								-> OK
+//Establir vertex						-----------------> Malament fet
+//Detectar col.lisions dels vertexs amb parets	-> OK
 //Establir nou estat després de col.lisió
 
 bool show_test_window = false;
@@ -28,7 +28,7 @@ bool reset = false;
 bool pause = false;
 
 glm::vec3 gravity{ 0.f, -9.81f, 0.f };
-glm::vec3 initPos{ 0.f, 10.f, 0.f };
+glm::vec3 initPos{ 0.f, 5.f, 0.f };
 
 glm::vec3 force;
 glm::vec3 cubePos;
@@ -40,10 +40,7 @@ glm::mat4 cubeMatRotZ = glm::mat4(1.f);
 
 float rx, ry, rz; //random angles rotation
 
-const float halfW = 0.5f; //tamany de la arista
-
-//glm::vec3 verts[];
-//glm::vec3 a[];
+float halfW = 0.5f; //tamany de la arista
 
 //   4---------7
 //  /|        /|
@@ -53,6 +50,7 @@ const float halfW = 0.5f; //tamany de la arista
 //| /       | /
 //|/        |/
 //1---------2
+
 glm::vec3 verts[] = {
 	cubePos + glm::vec3(-halfW, -halfW, -halfW),
 	cubePos + glm::vec3(-halfW, -halfW,  halfW),
@@ -97,7 +95,8 @@ void GUI() {
 		ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);//FrameRate
 
 		if (ImGui::Button("Reset Simulation"))	reset = true;
-		if (ImGui::Button("Pause"))	pause = true;
+
+		ImGui::Checkbox("Pause", &pause);
 	}
 
 	ImGui::End();
@@ -111,13 +110,23 @@ void GUI() {
 void PhysicsInit() {
 
 	cubePos = initPos;
-	force = glm::vec3{ static_cast <float> (rand() % 10 - 5), 
+	/*force = glm::vec3{ static_cast <float> (rand() % 10 - 5), 
 					   static_cast <float> (rand() % 10 - 5), 
-					   static_cast <float> (rand() % 10 - 5) };
+					   static_cast <float> (rand() % 10 - 5) };*/
 
-	rx = static_cast <float> (rand() % 1 - 0.5);
+	force = glm::vec3{
+		-5 + static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / (10))),
+		-5 + static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / (10))),
+		-5 + static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / (10)))
+	};
+
+	/*rx = static_cast <float> (rand() % 1 - 0.5);
 	ry = static_cast <float> (rand() % 1 - 0.5);
-	rz = static_cast <float> (rand() % 1 - 0.5);
+	rz = static_cast <float> (rand() % 1 - 0.5);*/
+
+	rx = -0.5 + static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / (1)));
+	ry = -0.5 + static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / (1)));
+	rz = -0.5 + static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / (1)));
 
 	Cube::setupCube();
 	Sphere::setupSphere();
@@ -125,85 +134,90 @@ void PhysicsInit() {
 
 void PhysicsUpdate(float dt) {
 	if (!pause) {
-	force += gravity * dt;
-	cubePos += force * dt;
+		force += gravity * dt;
+		cubePos += force * dt;
 
-	//mirar de fer-ho amb una sola variable, tal com está feta la camera dels projectes d inf grafica
-
-	cubeMatPos = glm::translate(glm::mat4(1.f), cubePos);
-
-	cubeMatRotX = glm::rotate(cubeMatRotX, rx, glm::vec3{ 1, 0, 0 });
-	cubeMatRotY = glm::rotate(cubeMatRotY, ry, glm::vec3{ 0, 1, 0 });
-	cubeMatRotZ = glm::rotate(cubeMatRotZ, rz, glm::vec3{ 0, 0, 1 });
-
-	//https://stackoverflow.com/questions/11253930/how-can-i-find-out-the-vertex-coordinates-of-a-rotating-cube
-	//v' = R * v     ---\/ actualizar vertex segons rotació
-
-	/*glm::vec3 verts[] = {
-		glm::vec4(cubePos, 0) + glm::vec4(-halfW, -halfW, -halfW, 0) * (cubeMatRotX * cubeMatRotY * cubeMatRotZ),
-		glm::vec4(cubePos, 0) + glm::vec4(-halfW, -halfW,  halfW, 0) * (cubeMatRotX * cubeMatRotY * cubeMatRotZ),
-		glm::vec4(cubePos, 0) + glm::vec4(halfW, -halfW,  halfW, 0)  * (cubeMatRotX * cubeMatRotY * cubeMatRotZ),
-		glm::vec4(cubePos, 0) + glm::vec4(halfW, -halfW, -halfW, 0)  * (cubeMatRotX * cubeMatRotY * cubeMatRotZ),
-		glm::vec4(cubePos, 0) + glm::vec4(-halfW,  halfW, -halfW, 0) * (cubeMatRotX * cubeMatRotY * cubeMatRotZ),
-		glm::vec4(cubePos, 0) + glm::vec4(-halfW,  halfW,  halfW, 0) * (cubeMatRotX * cubeMatRotY * cubeMatRotZ),
-		glm::vec4(cubePos, 0) + glm::vec4(halfW,  halfW,  halfW, 0)  * (cubeMatRotX * cubeMatRotY * cubeMatRotZ),
-		glm::vec4(cubePos, 0) + glm::vec4(halfW,  halfW, -halfW, 0)  * (cubeMatRotX * cubeMatRotY * cubeMatRotZ)
-	};*/
-
-	/*glm::vec3 a[] = {
-		glm::vec4(a[0], 0) * (cubeMatRotX * cubeMatRotY * cubeMatRotZ),
-		glm::vec4(a[1], 0) * (cubeMatRotX * cubeMatRotY * cubeMatRotZ),
-		glm::vec4(a[2], 0) * (cubeMatRotX * cubeMatRotY * cubeMatRotZ),
-		glm::vec4(a[3], 0) * (cubeMatRotX * cubeMatRotY * cubeMatRotZ),
-		glm::vec4(a[4], 0) * (cubeMatRotX * cubeMatRotY * cubeMatRotZ),
-		glm::vec4(a[5], 0) * (cubeMatRotX * cubeMatRotY * cubeMatRotZ),
-		glm::vec4(a[6], 0) * (cubeMatRotX * cubeMatRotY * cubeMatRotZ),
-		glm::vec4(a[7], 0) * (cubeMatRotX * cubeMatRotY * cubeMatRotZ)
-	};*/
-
-	/*glm::vec3 verts[] = {
-		cubePos + a[0],
-		cubePos + a[1],
-		cubePos + a[2],
-		cubePos + a[3],
-		cubePos + a[4],
-		cubePos + a[5],
-		cubePos + a[6],
-		cubePos + a[7],
-	};*/
-
-	a[0] = (cubeMatRotX * cubeMatRotY * cubeMatRotZ) * glm::vec4(a[0], 0);
-	verts[0] = a[0] + cubePos;
-
-	//std::cout << a[0].x << ", " << a[0].y << ", " << a[0].z << std::endl;
-
-	if (reset)
-	{
-		//reset things
-
-		cubePos = initPos;
-
-		force = glm::vec3{ static_cast <float> (rand() % 10 - 5), static_cast <float> (rand() % 10 - 5), static_cast <float> (rand() % 10 - 5) };
-
-		rx = static_cast <float> (rand() % 2 - 1);
-		ry = static_cast <float> (rand() % 2 - 1);
-		rz = static_cast <float> (rand() % 2 - 1);
+		//mirar de fer-ho amb una sola variable, tal com está feta la camera dels projectes d inf grafica
 
 		cubeMatPos = glm::translate(glm::mat4(1.f), cubePos);
+
 		cubeMatRotX = glm::rotate(cubeMatRotX, rx, glm::vec3{ 1, 0, 0 });
 		cubeMatRotY = glm::rotate(cubeMatRotY, ry, glm::vec3{ 0, 1, 0 });
 		cubeMatRotZ = glm::rotate(cubeMatRotZ, rz, glm::vec3{ 0, 0, 1 });
 
-		reset = false;
+		//https://stackoverflow.com/questions/11253930/how-can-i-find-out-the-vertex-coordinates-of-a-rotating-cube
+		//v' = R * v     ---\/ actualizar vertex segons rotació
+
+		for (int i = 0; i < 8; i++) {
+			a[i] = (cubeMatRotX * cubeMatRotY * cubeMatRotZ) * glm::vec4(a[i], 0);
+			verts[i] = a[i] + cubePos;
+		}
+
+		if (reset)	//reset things
+		{
+			cubePos = initPos;
+
+			//force = glm::vec3{ static_cast <float> (rand() % 10 - 5), static_cast <float> (rand() % 10 - 5), static_cast <float> (rand() % 10 - 5) };
+
+			force = glm::vec3{
+				-5 + static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / (10))),
+				-5 + static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / (10))),
+				-5 + static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / (10)))
+			};
+
+			//rx = static_cast <float> (rand() % 2 - 1);
+			//ry = static_cast <float> (rand() % 2 - 1);
+			//rz = static_cast <float> (rand() % 2 - 1);
+
+			rx = -0.5 + static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / (1)));
+			ry = -0.5 + static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / (1)));
+			rz = -0.5 + static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / (1)));
+
+			cubeMatPos = glm::translate(glm::mat4(1.f), cubePos);
+			cubeMatRotX = glm::rotate(cubeMatRotX, rx, glm::vec3{ 1, 0, 0 });
+			cubeMatRotY = glm::rotate(cubeMatRotY, ry, glm::vec3{ 0, 1, 0 });
+			cubeMatRotZ = glm::rotate(cubeMatRotZ, rz, glm::vec3{ 0, 0, 1 });
+
+			verts[0] = cubePos + glm::vec3(-halfW, -halfW, -halfW);
+			verts[1] = cubePos + glm::vec3(-halfW, -halfW, halfW);
+			verts[2] = cubePos + glm::vec3(halfW, -halfW, halfW);
+			verts[3] = cubePos + glm::vec3(halfW, -halfW, -halfW);
+			verts[4] = cubePos + glm::vec3(-halfW, halfW, -halfW);
+			verts[5] = cubePos + glm::vec3(-halfW, halfW, halfW);
+			verts[6] = cubePos + glm::vec3(halfW, halfW, halfW);
+			verts[7] = cubePos + glm::vec3(halfW, halfW, -halfW);
+
+			a[0] = glm::vec3(-halfW, -halfW, -halfW);
+			a[1] = glm::vec3(-halfW, -halfW, halfW);
+			a[2] = glm::vec3(halfW, -halfW, halfW);
+			a[3] = glm::vec3(halfW, -halfW, -halfW);
+			a[4] = glm::vec3(-halfW, halfW, -halfW);
+			a[5] = glm::vec3(-halfW, halfW, halfW);
+			a[6] = glm::vec3(halfW, halfW, halfW);
+			a[7] = glm::vec3(halfW, halfW, -halfW);
+
+			reset = false;
+		}
+
+////////COLLISION
+
+		for (int i = 0; i < 8; i++)
+		{
+			if (verts[i].x < -5)		pause = true;
+			else if (verts[i].x > 5)	pause = true;
+			else if (verts[i].y < 0)	pause = true;
+			else if (verts[i].y > 10)	pause = true;
+			else if (verts[i].z < -5)	pause = true;
+			else if (verts[i].z > 5)	pause = true;
+		}
+
+		Cube::updateCube(cubeMatPos * (cubeMatRotX * cubeMatRotY * cubeMatRotZ));	//REVISAR ABANS D'ENTREGAR
+
+		Sphere::updateSphere(verts[0], 0.1);	//vertex
 	}
 
-
-	Cube::updateCube(cubeMatPos * (cubeMatRotX * cubeMatRotY * cubeMatRotZ));	//REVISAR ABANS D'ENTREGAR
-
-
-	Sphere::updateSphere(verts[0], 0.1);
-	}
 	Cube::drawCube();
+
 	Sphere::drawSphere();
 }
 
